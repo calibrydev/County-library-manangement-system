b7daf2491ff846133c6298afe2fdc0356404f287
calibrydev <85285869+calibrydev@users.noreply.github.com>
2021-08-04 13:01:41 +0300

    Add files via upload
    Documented program

--- LIBRARY.C
+++ LIBRARY.C
@@ -1,5 +1,5 @@
 /*
-compiler :c99
+compiler :C99
 Project:Library management
 Author:Mark Mwaura
 Date:7/19/2021
@@ -9,14 +9,19 @@ License:MIT
 
 #include<stdio.h>
 #include<stdlib.h>
+#include<time.h>
 int choice;
+    //function prototypes used for the program
     void exit(); 
     void admin();
     void user();
     void staff();
-void action (){
-printf("\t\t\t\t\t\t\t COUNTY LIBRARY MANAGEMENT SYSTEM \n\n");
-//naviagtion menu
+    void books();
+    void actionMini();
+   //the action function that displas the Menu
+     void action (){
+    printf("\t\t\t\t\t\t\t COUNTY LIBRARY MANAGEMENT SYSTEM \n\n");
+    //naviagtion menu
     //loop used for looping when the user makes a mistake
     do{
     printf("\t\t\t\t\t\t\t+******************************+\n");
@@ -48,14 +53,10 @@ printf("\t\t\t\t\t\t\t COUNTY LIBRARY MANAGEMENT SYSTEM \n\n");
 
 
 };
-    void admin(){
-         
-        int choice;
-        int pin1;
-        printf("Enter pin");
-        scanf("%d",&pin1);
-
-        if(pin1==1212){
+    //this is  a Function for the Admin to add staff
+    void actionMini(){
+            int option;
+            int choice;
             char staffUser[20];
             char staffUser2[20];
 
@@ -68,26 +69,43 @@ printf("\t\t\t\t\t\t\t COUNTY LIBRARY MANAGEMENT SYSTEM \n\n");
             printf("Enter option:");
             scanf("%d",&choice);
             if(choice==1){
-
+                    //this shows the use of files to add staff members
                 FILE*fptr;
-               fptr = fopen("lib.txt","w");
-               printf("Add Staff name:");
-               scanf("%s",&staffUser);
-               printf("Add second Staff's name:");
-               scanf("%s",&staffUser2);
-     //after the user enters info it is now stored in the text file
-               fprintf(fptr,"%s",staffUser);
-               fprintf(fptr,"%s",staffUser2);
+                fptr = fopen("lib.txt","w");
+                printf("Add Staff name:");
+                scanf("%s",&staffUser);
+                printf("Add second Staff's name:");
+                scanf("%s",&staffUser2);
+     //after the admin enters info it is now stored in the text file
+                fprintf(fptr,"%s",staffUser);
+                fprintf(fptr,"%s",staffUser2);
     //closes the text file
-               fclose(fptr);
+                fclose(fptr);
 
-            }
+                printf("1 .Go to main menu\n2 for exit\n");
+                scanf("%d",&option);
+                if(option==1){
+                    action();
+                }
+                else 
+                exit();
+                }
 
             else if(choice==2){
-                
+                //deletss data from the text file
+                remove("library.txt");
+                  printf("1 .Go to main menu\n2 for exit\n");
+                scanf("%d",&option);
+                if(option==1){
+                    action();
+                }
+                else 
+                exit();
             }
             else if(choice==3){
                  FILE*fptr;
+
+                 //  this code  appends or changes the content of the file
                fptr = fopen("lib.txt","a");
                printf("Add Staff name:");
                scanf("%s",&staffUser);
@@ -98,33 +116,143 @@ printf("\t\t\t\t\t\t\t COUNTY LIBRARY MANAGEMENT SYSTEM \n\n");
                fprintf(fptr,"%s",staffUser2);
     //closes the text file
                fclose(fptr);
+                printf("1 .Go to main menu\n2 for exit\n");
+                scanf("%d",&option);
+                if(option==1){
+                    action();
+                }
+                else 
+                exit();
             }
-        else if (choice==4)
+             else if (choice==4)
+              printf("1 .Go to main menu\n2 for exit\n");
+                scanf("%d",&option);
+                if(option==1){
+                    action();
+                }
+                else 
+                exit();
+        };
+    void admin(){
+
+         int option;
+        int choice;
+        int pin1;
+        printf("Enter pin");
+        scanf("%d",&pin1);
+
+        if(pin1==1212){
+             //if the password is correct it calls the function that displays the menu to the admin
+                 actionMini();
+        }
+        else 
+            printf("Wrong password\n");
+                    
+    };
+    //shows the books availbale in the system
+    void books (){
+        //variables that are used in 
+    int option;
+    int optionz;
+    int counter;
+    int bookChoice[5];
+    int counter2;
+         FILE*fptr;
+         fptr=fopen("library.txt","w");
+         //it shows the books from the file
+         fprintf(fptr ,"1.Harry porter and the deadly hallows by r k rowlingg\n 2.understanding prayer by myles munroe\n 3.Prayer by Apostle Joshua selman\n");
+         fclose(fptr);
+         printf("Welcome\n");
+         printf("This is our list of books avaiable\n");
+        //asks how many books the user wants
+        printf("How many books do you want?\n");
+        scanf("%d",&optionz);
+        if(optionz >0 && optionz < 4){
+            //it allows the user to choose hwo many books he wants 
+    switch(optionz){
+        case 1:
+        printf("Choose a book\n");
+        scanf("%d",&bookChoice[0]);
+    //reserving a book
+       printf("Sorry but this book is reserved\n");
+            break;
+            case 2:
+             printf("Choose first book\n");
+        scanf("%d",&bookChoice[0]);
+         printf("Choose second book\n");
+        scanf("%d",&bookChoice[1]);
+        printf("You have succssesfully borrowed a book from us \n");
+       printf("Thank you for using our serivces\n");
+        break;
+        case 3:
+        printf("Choose first book\n");
+        scanf("%d",&bookChoice[0]);
+         printf("Choose second book\n");
+        scanf("%d",&bookChoice[1]);
+         printf("Choose third book\n");
+        scanf("%d",&bookChoice[2]);
+        printf("You have succssesfully borrowed a book from us \n");
+       printf("Thank you for using our serivces\n");
+        break;
+    }
+        }
+        else {
+        printf("You cannot borrow more than 4 boooks\n");
+   }
+   //it implements fine if the book is returned late
+     printf("You are to return the book before 20160 minutes or 2 weeks\n");
+        counter=20160;
+        do{
+            counter--;
+        }
+        while(counter!=0);
+        printf("Your time is over\n");
+      //it implements a fine of 1cent every time the user delays 
+        counter=1;
+        do{
+            counter2++;
+        }
+        while(counter2!=50); 
+        //gives user option of going back to the main menu
+        do{
+        printf("Enter 1 to go back to main menu\n Enter 2 to exit\n");
+        scanf("%d",&option);
+        if(option==1){
             action();
         }
+        else if(option==2){
+            exit();
+        }
+        else {
+            printf("INvalid\n");
+            printf("Please use 1 or 2\n");
+        }
 
-
-
-
-
-
-
-
+        
+        }while(option >2 || option < 1);
+        //loop to fine the user if the user exceeds the time
+       
+      
+        counter2=50;
+        
+    
 
     };
-void user(){
-   int bookChoice;
+   void user(){
+   int bookChoice[5];
     char fname[10];
     char sname[10];
     char tname[10];
     char details[40];
-    //int option;
+    int option;
     int password;
-
+        int optionz;
+        int phone,id;
+    //welcomes the user
     printf("Welcome dear user \n");
     printf("Log in \n");  
     printf("Enter details to log in\n");
-
+    //takes the details of the user
     printf("firstname:");
     scanf("%s",fname);
     printf("secondname:");
@@ -132,15 +260,22 @@ void user(){
     printf("thirdname:");
     scanf("%s",tname); 
     printf(" your name is %s %s %s\n",fname,sname,tname);
-    //printf("Enter password \n");
-    //scanf("%s",&password);
-    FILE*fptr;
-    fptr=fopen("library.txt","w");
-    fprintf(fptr ,"1.Harry porter and the deadly hallows by r k rowlingg\n 2.understanding prayer by myles munroe\n 3.Prayer by Apostle Joshua selman\n");
+    printf("Enter your librarary ID:");
+    scanf("%d",&id);
 
-    fclose(fptr);
-        printf("Welcome\n");
-        printf("This is our list of books avaiable\n");
+    //validates the password of the user if the password is  correct then it calls the menu function that  shows the list of available books
+        if(id==88411){
+            int pass=1212;
+             printf("Enter password\n");
+             scanf("%d",&password);
+             if(password==pass){
+                 printf("Correct Log in\n");
+                 books();
+             }   
+             else
+             printf("Wrong password\n");
+        }
+
    /* fptr=fopen("lib.txt","r");
     char buuk[200];
     while(!feof(fptr)){
@@ -149,6 +284,7 @@ void user(){
     }
     fclose(fptr);
   */
+ FILE*fptr;
     fptr=fopen("library.txt","r");
     char bouk[200];
     while(!feof(fptr)){
@@ -157,23 +293,12 @@ void user(){
     }
     fclose(fptr);
         
-        
-        
-  for (int i = 1; i < 4; ++i)
-  {
-        printf("Choose a book\n");
-        scanf("%d",&bookChoice);
-        if(i>3){
-            printf("You cannot take more than 3 books\n");
-        }
-  }
-    
-        
-
 
 };
+
 //this is a staff function that  defines the workings or functions of a staff  memeber
 void staff(){
+
     //declaration of variables
     int ans;
     char book[10];
@@ -185,15 +310,10 @@ void staff(){
     printf("1. Add book\n");
     printf("2. delete book\n");
     printf("3. edit book\n");
-    printf("4. lend book\n");
-    printf("5. Add book\n");
-    printf("6. mark returned book\n");
-    printf("7. fine user if the book is returned late \n");
-    printf("8. limit the number of books a user can borrow at a time to 3\n");
-    printf("9. mark a book as reserved\n");
-    printf("10.Add user\n");
-    printf("11.edit user\n");
-    printf("12.delete user\n");
+    printf("4. Add book\n");
+    printf("5.Add user\n");
+    printf("6.edit user\n");
+    printf("7.delete user\n");
 
 
     printf("Enter option:");
@@ -222,24 +342,8 @@ void staff(){
    //closes the text file
    fclose(fptr);
 break;
-    
-    case 2:
-    break; 
-    case 3:
-    break; 
-    case 4:
-    break; 
+    //adds users
     case 5:
-    break;
-    case 6:
-    break;
-    case 7:
-    break;
-    case 8:
-    break;
-    case 9:
-    break;    
-    case 10:
     
     //FILE*fptr;
     fptr = fopen("lib.txt","w");
@@ -286,51 +390,16 @@ break;
 
 
 };
+
 //this is the code for the administrator
-/*void admin(){
-    int pinadmin=1234;
-    printf("Enter password\n");
-    scanf("%d",&pinadmin);
-    if(pinadmin=1234){
-    printf("1.Add staff\n");
-    printf("1.delete user\n");
 
-
-    }
-
-};*/
 void exit(){
     printf("Thank you for visiting us \n");
-   // _sleep(5000);
     
 };
+    //the main function
 int main(){
-     printf("\t\t\t\t\t\t\t COUNTY LIBRARY MANAGEMENT SYSTEM \n\n");
-//naviagtion menu
-    //loop used for looping when the user makes a mistake
-    /*do{
-    printf("\t\t\t\t\t\t\t+******************************+\n");
-    printf("\t\t\t\t\t\t\t|            LOG IN AS:        |\n");
-    printf("\t\t\t\t\t\t\t|            1.USER            |\n");
-    printf("\t\t\t\t\t\t\t|            2.Staff           | \n");    
-    printf("\t\t\t\t\t\t\t|            3.Administrator   |\n");
-    printf("\t\t\t\t\t\t\t|            4.exit            |\n");
-    printf("\t\t\t\t\t\t\t+******************************+\n");
-    printf("\t\t\t\t\t\t\t Enter choice :");
-    scanf("%d",&choice);
-    }while(choice <1 || choice >4);
-
-        if(choice==1){
-            user();
-        }  
-        else if(choice==2){
-            staff();
-        }
-        
-        else if(choice==4){
-            exit();
-        }
-    */
+// function for the navigation men
    action();
     
 
